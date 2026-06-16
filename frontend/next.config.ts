dc73756f60f84d137c17e9874806efc20145ca3d
Nurzigit <n18983084@gmail.com>
2026-06-16 14:56:43 +0600

    fix prefrght

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        // Перехватываем все запросы к /api и перенаправляем на бэкенд на уровне Vercel
+        source: '/api/:path*',
+        destination: 'https://future-techbackend-96v27nfgu-play-b-s-projects.vercel.app/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
